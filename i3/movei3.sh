70618a1fa8cd57ba295e9e016762e257ad01b4a7
Philip Johansson <philipphuket@gmail.com>
2021-01-03 18:42:14 +0100

    work in progress

--- i3/movei3.sh
+++ i3/movei3.sh
@@ -5,5 +5,6 @@ ln -sf ~/.dotfiles/i3/i3status/ ~/.config/ || echo "could not move i3status"
 ln -sf ~/.dotfiles/i3/picom.conf ~/.config/ || echo "could not move picom conf"
 ln -sf ~/.dotfiles/i3/rofi/ ~/.config/ || echo "could not move rofi conf"
 ln -sf ~/.dotfiles/i3/.xinitrc ~/ || echo "could not move xinitrc"
+ln -sf ~/.dotfiles/i3/.Xresources ~/ || echo "could not move Xresources"
 
 echo "linked i3 files"
