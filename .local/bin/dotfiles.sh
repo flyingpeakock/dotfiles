ec78d0cedbd1bb25b163957398bbe57433ceab46
Philip Johansson <philipphuket@gmail.com>
2021-04-19 22:26:30 +0200

    updated install script

--- .local/bin/dotfiles.sh
+++ .local/bin/dotfiles.sh
@@ -21,15 +21,14 @@ setup() {
     fi;
     gitdot checkout
     gitdot config status.showUntrackedFiles no
+
+    echo "Don't forget to change shell and set setxkbmap"
 }
 
 [ ! -e $repoDir ] && setup
 
 sudo ln -sf ~/Documents/systemd/backup.timer /etc/systemd/system/ || echo "Could not link backup.timer"
-
 sudo ln -sf ~/Documents/systemd/backup.service /etc/systemd/system/ || echo "Could not link backup.service"
-
 sudo ln -sf ~/Documents/systemd/customlock@.service /etc/systemd/system/ || echo "Could not link custom lock"
-
 sudo ln -sf ~/.config/xkb/se_cm /usr/share/X11/xkb/symbols/ || echo "Could not link xkb map"
 
