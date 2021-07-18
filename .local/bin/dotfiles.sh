a08cc4dfe81c4ba3fedef20326546d809ef674e0
Philip Johansson <philipphuket@gmail.com>
2021-07-18 11:11:37 +0200

    fixed setup script to actually move the conflicting files

--- .local/bin/dotfiles.sh
+++ .local/bin/dotfiles.sh
@@ -1,45 +1,53 @@
 #!/bin/sh
-# This is a currently just a script that links certain things to
-# outside of my home directory
+
+# This script clones a bare reposity then checks it out
+# into the users home directory. It also links a swedish
+# colemak keyboard layout as well as enables it.
+# Author: Philip Johansson
 
 repoDir=$HOME/.config/repo.git
-backupDir=$HOME/backup-config
 gitURL="https://github.com/flyingpeakock/dotfiles.git"
+backupDir=$HOME/config.backup
+
 
 gitdot() {
     git --git-dir=$repoDir --work-tree=$HOME $@
 }
 
+backup() {
+    dir=$(dirname $2)
+    printf "\tBacking up file $1\n"
+    mkdir -p $dir
+    mv $1 $2
+}
+export -f backup
+
 setup() {
     printf "Cloning dotfiles bare repo into $repoDir\n"
-    git clone --bare $gitURL $repoDir > /dev/null
+    git clone --bare $gitURL $repoDir > /dev/null 2>&1
     printf "Attempting to check out bare repo\n"
-    gitdot checkout 2>&1 /dev/null
+    gitdot checkout > /dev/null 2>&1
     if [ $? -ne 0 ]; then
-        mkdir -p $backupDir
-        printf "Conflicting config files found. Moving to $backupDir\n"
-        gitdot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $backupDir/{}
+        printf "Conflicting files found. Moving to $backupDir\n"
+        gitdot checkout 2>&1 | egrep "^\s" | awk {'print $1'} | xargs -I{} -- sh -c 'backup "{}" '"$backupDir"'/"{}"'
     fi;
     gitdot checkout > /dev/null && printf "Checked out bare repo to home directory\n"
     gitdot config status.showUntrackedFiles no > /dev/null
-}
 
-changeShell() {
-    printf "Change default shell to zsh?\n"
-    select yn in "Yes" "No"
-    case $yn in
-        Yes ) chsh -s /bin/zsh;;
-    esac
+    printf "Adding plugins as submodules\n"
+    gitdot submodule init > /dev/null 2>&1
+    printf "Updating plugins\n"
+    gitdot submodule update > /dev/null 2>&1
 }
 
 setkeyMap() {
-    sudo ln -sf ~/.config/xkb/se_cm /usr/share/X11/xkb/symbols/ 2>&1 /dev/null
-    setxkbmap se_cm 2>&1 /dev/null
+    [ ! -e /usr/share/X11/xkb/symbols/se_cm ] && sudo ln -sf ~/.config/xkb/se_cm /usr/share/X11/xkb/symbols/ > /dev/null 2>&1
+    setxkbmap se_cm > /dev/null 2>&1
 }
-
 
 [ ! -e $repoDir ] && setup
 
-shell = `echo $SHELL | awk -F '/' '{print $(NF)}'`
-[ $shell = "zsh" ] || changeShell
-[ -e $HOME/.config/xkb/se_cm ] && setkeyMap || printf "Error setting xkb map. Perhaps you don't have X installed?\n"
+shell=$(echo $SHELL | awk -F '/' '{print $(NF)}')
+[ "$shell" != "zsh" ] && chsh -s /bin/zsh
+[ -e $HOME/.config/xkb/se_cm ] && setkeyMap
+
