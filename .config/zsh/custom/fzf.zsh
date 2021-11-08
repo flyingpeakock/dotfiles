d2f733cee478a948ebc1e575731804e700e8e12e
Philip Johansson <philipphuket@gmail.com>
2021-11-08 21:41:43 +0100

    fixed fzf-down to _FZF_COMMAND

--- .config/zsh/custom/fzf.zsh
+++ .config/zsh/custom/fzf.zsh
@@ -103,7 +103,7 @@ fgco () {
 fgh () {
   is_in_git_repo || return
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
-  fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+  _FZF_COMMAND --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
     --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always' |
   grep -o "[a-f0-9]\{7,\}"
